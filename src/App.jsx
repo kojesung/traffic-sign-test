b974039b71764fe82577073afe357e4e4168c332
kojesung <js95112345@gmail.com>
2024-07-30 16:39:46 +0900

    useTab 기능 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
 import { useState } from 'react';
 import useInput from './useInput';
+import useTab from './useTab';
 
 function App() {
     const maxLength = (value) => value.length < 10; //유효성 검사할 함수
     const name = useInput('Mr. ', maxLength); //return되는 value와 onChange를 사용할 수 있음
+    /////////////////////////////////////////////////////////
+    const content = [
+        {
+            tab: '첫번째 탭',
+            content: '이것은 첫번째 탭',
+        },
+        {
+            tab: '두번째 탭',
+            content: '이것은 두번째 탭',
+        },
+    ];
+    const { currentItem, changeItem } = useTab(0, content);
+    //currentItem은 현재 나타낼 값
+    //changeItem은 현재 나타낼 값을 바꿀 수 있게 해주는 함수
+    //currentItem이 index값에 따라서 바뀌는 것이기 때문에 changeItem에 index값을 넘겨주며 currentItem을 바꿔줌
+    //
+
     return (
         <div>
-            <h1>Hello</h1>
-            <input placeholder="Name" {...name} />
-            {/* value={name.value} onChange={name.onChange} 과 같은 뜻임*/}
+            <div>
+                <h1>useInput 기능</h1>
+                <input placeholder="Name" {...name} />
+                {/* value={name.value} onChange={name.onChange} 과 같은 뜻임*/}
+            </div>
+            <div>
+                <h1>useTab 기능</h1>
+                {content.map((currentTab, index) => (
+                    <button
+                        onClick={() => {
+                            changeItem(index);
+                        }}
+                    >
+                        {currentTab.tab}
+                    </button>
+                ))}
+                <div>{currentItem.content}</div>
+                {/* 
+                처음에 만드는 도중 tabResult를 그대로 반환하려고 해서 에러가 났는데 이 결과값을 눈으로 볼 수 없어서 찾는데 오래걸렸음
+                JSON.stringly로 렌더링할 값을 감싸서 어떤 형태로 나오는지 확인할 수 있었음
+                 */}
+            </div>
+            <div>
+                <h1></h1>
+            </div>
         </div>
     );
 }
---
+++ src/useTab.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const useTab = (initialTab, allTabs) => {
+    const [currentIndex, setCurrentIndex] = useState(initialTab);
+    if (!allTabs || !Array.isArray(allTabs)) {
+        return;
+    }
+    return { currentItem: allTabs[currentIndex], changeItem: setCurrentIndex };
+    //changeItem이라는 반환값에 setCurrentIndex담음
+    //serCurrentIndex라는 함수 자체를 빌려주어서 import하는 곳에서 사용할 수 있는데
+    //javascript에서는 함수 자체가 일급 객체이기 때문에 가능
+    //javascript의 객체 리터럴 방식을 활용한 반환값인데
+    // const a = {
+    //     currentItem : allTabs[currentIndex],
+    //     changeItem : setCurrentIndex
+    // }
+    //이렇게 a를 반환해준다는 것과 같은 뜻임
+};
+
+export default useTab;
