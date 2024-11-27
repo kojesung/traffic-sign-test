83d4f2498561fee0e3754294e5a4696ad9e0cce1
kojesung <js95112345@gmail.com>
2024-11-27 20:19:05 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/traffic-sign-test/', // GitHub repository 이름과 정확히 일치
+    base: '/traffic-sign-test/',
     plugins: [react()],
+    build: {
+        outDir: './', // 루트 디렉토리에 빌드
+        assetsDir: 'assets', // 에셋 파일 디렉토리
+    },
 });
