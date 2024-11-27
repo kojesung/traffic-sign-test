0c528cc43d4538cb3449d868c49622f6c4c449c7
kojesung <js95112345@gmail.com>
2024-11-27 20:03:42 +0900

    Rebuild for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: './', // 이 부분을 변경
+    base: '/traffic-sign-test/', // GitHub repository 이름과 정확히 일치
     plugins: [react()],
-    build: {
-        rollupOptions: {
-            output: {
-                assetFileNames: (assetInfo) => {
-                    // 원본 파일 이름 유지
-                    return `assets/${assetInfo.name}`;
-                },
-            },
-        },
-    },
 });
