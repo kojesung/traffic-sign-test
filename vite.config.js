cc1022e20a12e1e6c28c02d1ffce2aa7ff04c75a
kojesung <js95112345@gmail.com>
2024-11-27 15:55:47 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     base: '/traffic-sign-test/',
     plugins: [react()],
-    build: {
-        assetsDir: 'assets',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.json'],
-    },
 });
