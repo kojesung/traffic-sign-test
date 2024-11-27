f8946dceed4d6fb224f0a5d6b516e760e4bc5463
kojesung <js95112345@gmail.com>
2024-11-27 15:45:40 +0900

    build : 이미지 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
-    base: '/traffic-sign-test/',
+    base: './', // './'로 수정
     plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
-    },
     build: {
         assetsDir: 'assets',
-        rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, 'index.html'),
-            },
-            output: {
-                assetFileNames: 'assets/[name].[ext]',
-            },
-        },
     },
 });
