ee07a65fe1c1a66d2ae80721476da6952f3e85c1
kojesung <js95112345@gmail.com>
2024-11-27 15:50:27 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: './', // 상대 경로로 변경
+    base: '/traffic-sign-test/', // GitHub Pages의 repository 이름으로 다시 변경
     plugins: [react()],
     build: {
-        outDir: 'dist', // dist 폴더에 빌드
         assetsDir: 'assets',
-        rollupOptions: {
-            output: {
-                // 에셋 파일 이름 형식 지정
-                assetFileNames: 'assets/[name][extname]',
-                // 청크 파일 이름 형식 지정
-                chunkFileNames: 'assets/[name].js',
-                // 메인 엔트리 파일 이름 형식 지정
-                entryFileNames: 'assets/[name].js',
+    },
+    esbuild: {
+        loader: 'jsx',
+        include: /\.[jt]sx?$/,
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                '.js': 'jsx',
             },
         },
     },
